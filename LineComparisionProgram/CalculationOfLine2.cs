cc2d3e92b7f9e6304d2b6bb9c9b0382ef002047f
Nepolean23 <nlean1996@gmail.com>
2023-01-03 13:08:08 +0530

    [Nepolean]-Calculation of Line2

---
+++ LineComparisionProgram/CalculationOfLine2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionProgram
+{
+    public class CalculationOfLine2
+    {
+        public static void Line2()
+        {
+            int p, q, r, s, length2;
+            Console.WriteLine("Calculation of 2nd Line length");
+            Console.WriteLine("Enter a value for p,q,r,s");
+            Console.Write("p :");
+            p = Convert.ToInt32(Console.ReadLine());
+            Console.Write("q :");
+            q = Convert.ToInt32(Console.ReadLine());
+            Console.Write("r :");
+            r = Convert.ToInt32(Console.ReadLine());
+            Console.Write("s :");
+            s = Convert.ToInt32(Console.ReadLine());
+            length2 = (int)Math.Sqrt((p - q) ^ 2 + (r - s) ^ 2);
+            Console.WriteLine("The value of Length2 :{0}", length2);
+
+        }
+    }
+}
--- LineComparisionProgram/Program.cs
+++ LineComparisionProgram/Program.cs
@@ -1,3 +1,6 @@
 ﻿using LineComparisionProgram;
-
+// Calculation of 1stLine
 CalculationOfLine1.Line1();
+
+//Calculation of 2ndLine
+CalculationOfLine2.Line2();
